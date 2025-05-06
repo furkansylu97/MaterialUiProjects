061733cbf43eca45b64a631a8ff4c807d67cb2e7
Furkan Soylu <furkansylu97@gmail.com>
2025-05-06 16:52:48 +0300

    Printing text on the screen was performed according to the event

--- MaterialUiProjects/src/components/LessonTextField.tsx
+++ MaterialUiProjects/src/components/LessonTextField.tsx
@@ -1,6 +1,10 @@
-import { Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+import { Stack, TextField, InputAdornment } from '@mui/material';
 
 export const LessonTextField = () => {
+
+    const [value, setValue] = useState('')
+
   return (
     <>
       <Stack spacing={4} style={{ padding: 200 }}>
@@ -14,7 +18,34 @@ export const LessonTextField = () => {
             <TextField type='password' label='name' variant='outlined' helperText='Please enter your name'/>
             <TextField disabled type='password' label='name' variant='outlined'/>  {/* The button is pale and We couldn't write anything. (disabled) */}
             <TextField InputProps={{readOnly: true}} type='password' label='name' variant='outlined'/> {/* The button is bright and We couldn't write anything. */}
+            <TextField 
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position='start'>₺</InputAdornment>
+                )
+            }} 
+            label='Price' 
+            variant='outlined'/>
+
+            <TextField 
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position='end'>₺</InputAdornment>
+                )
+            }} 
+            label='Price' 
+            variant='outlined'/>
          </Stack>
+
+        <Stack>
+            <TextField 
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            type='password'
+            label='Password' 
+            variant='outlined'
+            helperText={!value ? 'Please enter your password' : 'Please never share your password with anyone'}/>
+        </Stack>
       </Stack>
     </>
   )
