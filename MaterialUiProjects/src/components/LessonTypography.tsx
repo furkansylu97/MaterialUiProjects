6e5547c9a9ef79e7602222c174a2767aa8118f4c
Furkan Soylu <furkansylu97@gmail.com>
2025-05-06 13:48:01 +0300

    Variant h1,h2,... works were performed

--- MaterialUiProjects/src/components/LessonTypography.tsx
+++ MaterialUiProjects/src/components/LessonTypography.tsx
@@ -5,6 +5,15 @@ export const LessonTypography = () => {
     <div>
         <Typography variant='body1'>Body1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, maxime. Quibusdam harum iste repellendus nulla nisi error nobis accusamus quas, ut omnis nostrum sapiente. Distinctio ullam illum minus harum blanditiis unde esse architecto. Consequatur, rem.</Typography>
         <Typography variant='body2'>Body2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, maxime. Quibusdam harum iste repellendus nulla nisi error nobis accusamus quas, ut omnis nostrum sapiente. Distinctio ullam illum minus harum blanditiis unde esse architecto. Consequatur, rem.</Typography>
+        
+        <Typography variant='h1'>H1 HELLO</Typography>
+        <Typography variant='h2'>H2 HELLO</Typography>
+        <Typography variant='h3'>H3 HELLO</Typography>
+        <Typography variant='h4'>H4 HELLO</Typography>
+        <Typography variant='h5'>H5 HELLO</Typography>
+        <Typography variant='h6'>H6 HELLO</Typography>
+
+        
     </div>
   )
 }
