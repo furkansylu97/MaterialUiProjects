5607e75d73ec26ddf395240567549f522a526417
Furkan Soylu <furkansylu97@gmail.com>
2025-05-06 22:33:19 +0300

    In the onChange event state, the process of getting the values ​​was performed

--- MaterialUiProjects/src/components/LessonCheckbox.tsx
+++ MaterialUiProjects/src/components/LessonCheckbox.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useState } from "react";
 import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, MenuItem, TextField } from "@mui/material";
 
 export const LessonCheckbox = () => {
+
+    const [isAccept, setIsAccept] = useState(false);
+    // console.log(isAccept);
+
   return (
     <>
       <Box>
         <FormControl>
             <FormLabel>Education Status</FormLabel>
-            <FormGroup>
-                <FormControlLabel control={ <Checkbox/> } label='elementary school' />
-                <FormControlLabel control={ <Checkbox/> } label='High school' />
+            <FormGroup row>
+                <FormControlLabel disabled control={ <Checkbox/> } label='elementary school' />
+                <FormControlLabel control={ <Checkbox defaultChecked/> } label='High school' /> {/* defaultChecked value is automatic selected value*/}
                 <FormControlLabel control={ <Checkbox/> } label='University' />
+            </FormGroup>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+            <FormLabel>Education Status</FormLabel>
+            <FormGroup row>
+                <FormControlLabel control={ <Checkbox checked= { isAccept } onChange={(e) => setIsAccept(e.target.checked)}/> } label='I accept the terms of use' />
             </FormGroup>
         </FormControl>
       </Box>
