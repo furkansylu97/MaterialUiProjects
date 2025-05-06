42c7d97d4db8798a6b698c80f37a9ddc07035753
Furkan Soylu <furkansylu97@gmail.com>
2025-05-06 22:05:26 +0300

    Select multiple selection operations were performed

--- MaterialUiProjects/src/components/LessonSelect.tsx
+++ MaterialUiProjects/src/components/LessonSelect.tsx
@@ -3,17 +3,35 @@ import { Box, MenuItem, TextField } from "@mui/material";
 
 export const LessonSelect = () => {
 
-    const [country, setCountry] = useState('');
+    const [country, setCountry] = useState<string[]>([]);
     console.log(country);
 
     return (
+        <>
+            {/* <Box width='150px'>
+                <TextField label='Select Country' select fullWidth value={country} onChange={(e) => setCountry(e.target.value)}>
+                    <MenuItem value='TR'>Turkey</MenuItem>
+                    <MenuItem value='ABD'>United States</MenuItem>
+                    <MenuItem value='FR'>France</MenuItem>
+                    <MenuItem value='CAN'>Canada</MenuItem>
+                </TextField>
+            </Box> */}
+
             <Box width='150px'>
-                <TextField label='Select Country' select fullWidth value={country} onChange={(e) => setCountry(e.target.value)}> {/* default value of select is true */}
+                <TextField 
+                SelectProps={{multiple: true,}} 
+                label='Select Country' 
+                select fullWidth value={country} 
+                onChange={(e) => setCountry(typeof e.target.value === 'string' 
+                    ? e.target.value.split(',')
+                    : e.target.value)
+                }>
                     <MenuItem value='TR'>Turkey</MenuItem>
                     <MenuItem value='ABD'>United States</MenuItem>
                     <MenuItem value='FR'>France</MenuItem>
                     <MenuItem value='CAN'>Canada</MenuItem>
                 </TextField>
             </Box>
+        </>
     );
 };
