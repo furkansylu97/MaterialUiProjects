caa197f24a9711e161833e62045bec168e94a7e8
Furkan Soylu <furkansylu97@gmail.com>
2025-05-07 15:20:54 +0300

    Control of the drop-down menu was achieved with the anchor element

--- MaterialUiProjects/src/components/LessonAppBar.tsx
+++ MaterialUiProjects/src/components/LessonAppBar.tsx
@@ -1,7 +1,18 @@
-import { AppBar, Toolbar, IconButton, Typography, Stack, Button,} from '@mui/material'
+import React from 'react';
+import { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem,} from '@mui/material'
 import AppsIcon from '@mui/icons-material/Apps';
 
 export const LessonAppBar = () => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const openControl = Boolean(anchorEl)
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    }
+
   return (
     <AppBar>
         <Toolbar>
@@ -14,7 +25,12 @@ export const LessonAppBar = () => {
                     <Button sx={{color: 'white'}}>ABOUT</Button>
                     <Button sx={{color: 'white'}}>PRODUCT</Button>
                     <Button sx={{color: 'white'}}>PRICE</Button>
+                    <Button sx={{color: 'white'}}  onClick={handleClick}>SALES</Button>
                 </Stack>
+                <Menu anchorEl={anchorEl} open={openControl}>
+                    <MenuItem>Books</MenuItem>
+                    <MenuItem>Pencils</MenuItem>
+                </Menu>
         </Toolbar>
     </AppBar>
   )
