a6e978e86349b5c10f11ef9cf4a518babcc41db6
Furkan Soylu <furkansylu97@gmail.com>
2025-05-08 00:24:43 +0300

    When the icon is clicked, the active feature operations were performed

--- MaterialUiProjects/src/components/LessonBottomNavigation.tsx
+++ MaterialUiProjects/src/components/LessonBottomNavigation.tsx
@@ -2,10 +2,16 @@ import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useState } from 'react';
 
 export const LessonBottomNavigation = () => {
+
+    const [value, setValue] = useState(0);
+
   return (
-    <BottomNavigation showLabels sx={{width: '100%', position: 'absolute', bottom: 0}}>
+    <BottomNavigation value={value} showLabels sx={{width: '100%', position: 'absolute', bottom: 0}} onChange={(_event, newValue) => {
+        setValue(newValue)
+    }}>
         <BottomNavigationAction icon={ <RestoreIcon/> } label='Recents'/>
         <BottomNavigationAction icon={ <FavoriteIcon/> } label='Favorites'/>
         <BottomNavigationAction icon={ <LocationOnIcon/> } label='Nearby'/>
