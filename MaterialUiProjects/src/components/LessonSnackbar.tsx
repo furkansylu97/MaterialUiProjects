485d00bd9bc90fb2672ef73133c3814a3910df1f
Furkan Soylu <furkansylu97@gmail.com>
2025-05-08 10:16:10 +0300

    Snackbar contents were written

--- MaterialUiProjects/src/components/LessonSnackbar.tsx
+++ MaterialUiProjects/src/components/LessonSnackbar.tsx
@@ -1,20 +1,28 @@
-import { useState } from 'react';
-import { Button, Snackbar } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-
+import React from "react";
+import { useState } from "react";
+import { Button, IconButton, Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const LessonSnackbar = () => {
-
-    const [openSnackbar, setOpenSnackbar] = useState(false)
+    const [openSnackbar, setOpenSnackbar] = useState(false);
 
     const handleClick = () => {
-        setOpenSnackbar(true)
-    }
+        setOpenSnackbar(true);
+    };
 
-  return (
-    <>
-      <Button onClick={handleClick}>Open Snackbar</Button>
-      <Snackbar open={openSnackbar} />
-    </>
-  )
-}
+    const action = (
+        <React.Fragment> {/* React Fragment tag cannot be defined as <></> in a variable */} 
+            <Button color="secondary" size="small">Undo</Button>
+            <IconButton>
+                <CloseIcon sx={{color: 'white'}} />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    return (
+        <>
+            <Button onClick={handleClick}>Open Snackbar</Button>
+            <Snackbar open={openSnackbar} message="You received an error message" action={action} />
+        </>
+    );
+};
