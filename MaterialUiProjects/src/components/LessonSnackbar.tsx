ad24259de7d58589e7540dc50e82693e2df85693
Furkan Soylu <furkansylu97@gmail.com>
2025-05-08 10:27:28 +0300

    Closing  method was written an Snackbar position were editted with anchorOrigin

--- MaterialUiProjects/src/components/LessonSnackbar.tsx
+++ MaterialUiProjects/src/components/LessonSnackbar.tsx
@@ -10,11 +10,18 @@ export const LessonSnackbar = () => {
         setOpenSnackbar(true);
     };
 
+    const handleClose = () => {
+        setOpenSnackbar(false);
+    };
+
     const action = (
-        <React.Fragment> {/* React Fragment tag cannot be defined as <></> in a variable */} 
-            <Button color="secondary" size="small">Undo</Button>
+        <React.Fragment>
+            {/* React Fragment tag cannot be defined as <></> in a variable */}
+            <Button color="secondary" size="small" onClick={handleClose}>
+                Undo
+            </Button>
             <IconButton>
-                <CloseIcon sx={{color: 'white'}} />
+                <CloseIcon sx={{ color: "white" }} onClick={handleClose} />
             </IconButton>
         </React.Fragment>
     );
@@ -22,7 +29,14 @@ export const LessonSnackbar = () => {
     return (
         <>
             <Button onClick={handleClick}>Open Snackbar</Button>
-            <Snackbar open={openSnackbar} message="You received an error message" action={action} />
+            <Snackbar
+                open={openSnackbar}
+                message="You received an error message"
+                action={action}
+                autoHideDuration={2000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            />
         </>
     );
 };
